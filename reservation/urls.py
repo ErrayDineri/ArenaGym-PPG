67f31befd7e52c75e3562e284177fbd2b68a4db2
Rayen Guermazi <guermazimaram@yahoo.fr>
2025-05-05 11:09:59 +0100

    .

--- reservation/urls.py
+++ reservation/urls.py
@@ -9,13 +9,10 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('booking/', views.bookingPage, name='booking'),
     path('client/', views.clientPage, name='client'),
-    path('booking/<int:court_id>/', views.bookingPage, name='booking_with_id'),  # new
+    path('booking/<int:court_id>/', views.bookingPage, name='booking_with_id'),
     path('coach/', views.coach_dashboard, name='coachdashboard'),
     path('coach/sessions/', views.coach_sessions, name='coachsessions'),
     path('coach/sessions/<int:session_id>/', views.session_detail, name='session_detail'),
     path('coach/sessions/<int:session_id>/cancel/', views.cancel_session, name='cancel_session'),
-    path('coach/students/', views.coach_students, name='coach_students'),
-
-
-
+    path('coach/students/', views.coach_students, name='coach_students')
 ]
